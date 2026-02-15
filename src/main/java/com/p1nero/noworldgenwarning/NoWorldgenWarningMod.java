52da1b958a21fcdadbd3536f6e44e252e930f4fb
P1nero <1070402601@qq.com>
2026-02-15 21:14:14 +0800

    initial commit

---
+++ src/main/java/com/p1nero/noworldgenwarning/NoWorldgenWarningMod.java
@@ -0,0 +1,12 @@
+package com.p1nero.noworldgenwarning;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(NoWorldgenWarningMod.MOD_ID)
+public class NoWorldgenWarningMod {
+
+    public static final String MOD_ID = "noworldgenwarning";
+    public NoWorldgenWarningMod() {
+    }
+
+}
---
+++ src/main/java/com/p1nero/noworldgenwarning/mixin/DensityFunctionsMixin.java
@@ -0,0 +1,17 @@
+package com.p1nero.noworldgenwarning.mixin;
+
+import net.minecraft.world.level.levelgen.DensityFunctions;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(DensityFunctions.TwoArgumentSimpleFunction.class)
+public interface DensityFunctionsMixin {
+
+    @Redirect(method = "create", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;)V"))
+    private static void tcr$warn(Logger instance, String s) {
+
+    }
+
+}
